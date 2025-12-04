b5d763952e7a5df3b82f03ffa3bdab581a86471a
majedmaher <43596895+majedmaher@users.noreply.github.com>
2025-12-04 14:14:43 +0200

    Work Page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,9 +1,155 @@
-import React from 'react'
+'use client'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import {Swiper, SwiperSlide} from "swiper/react";
+import 'swiper/css'
+import WorkSliderBtns from "@/components/WorkSliderBtns";
+const projects = [
+  {
+    num: "01",
+    category: "backend",
+    title: "Enjoy Games",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ducimus nemo dicta eius suscipit corrupti provident numquam dolorem possimus dignissimos nostrum qui et magni exercitationem, expedita inventore voluptate, modi repellat.",
+    stack: [{ name: "Laravel" }],
+    image: "/assets/work/thumb1.png",
+    live: "",
+    github: "https://github.com/majedmaher/smart_digital_shop",
+  },
+  {
+    num: "02",
+    category: "fullstack",
+    title: "Digo",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ducimus nemo dicta eius suscipit corrupti provident numquam dolorem possimus dignissimos nostrum qui et magni exercitationem, expedita inventore voluptate, modi repellat.",
+    stack: [{ name: "Laravel" },{name: 'Html 5'},{ name: "Css 3" },{name: 'Javascript'},{ name: "JQuery" },{name: 'AJAX'}],
+    image: "/assets/work/thumb2.png",
+    live: "",
+    github: "https://github.com/majedmaher/digo",
+  },
+  {
+    num: "03",
+    category: "mobile",
+    title: "Digo",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ducimus nemo dicta eius suscipit corrupti provident numquam dolorem possimus dignissimos nostrum qui et magni exercitationem, expedita inventore voluptate, modi repellat.",
+    stack: [{ name: "Flutter" }],
+    image: "/assets/work/thumb3.png",
+    live: "",
+    github: "https://github.com/majedmaher/digo_app_flutter",
+  },
+  {
+    num: "04",
+    category: "fullstack",
+    title: "PGCC",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ducimus nemo dicta eius suscipit corrupti provident numquam dolorem possimus dignissimos nostrum qui et magni exercitationem, expedita inventore voluptate, modi repellat.",
+    stack: [{ name: "Laravel" },{ name: "Livewire" },{name: 'Html 5'},{ name: "Css 3" },{name: 'Javascript'}],
+    image: "/assets/work/thumb1.png",
+    live: "",
+    github: "https://github.com/majedmaher/pgcc-app",
+  },
+  {
+    num: "05",
+    category: "fullstack",
+    title: "Najiz",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ducimus nemo dicta eius suscipit corrupti provident numquam dolorem possimus dignissimos nostrum qui et magni exercitationem, expedita inventore voluptate, modi repellat.",
+    stack: [{ name: "Laravel" },{ name: "Livewire" },{name: 'Html 5'},{ name: "Css 3" },{name: 'Javascript'}],
+    image: "/assets/work/thumb2.png",
+    live: "",
+    github: "https://github.com/majedmaher/najiz-law-app",
+  },
+];
 
 function Works() {
-  return (
-    <div>Works</div>
-  )
+  const [project, setProject] = useState(projects[0])
+
+  const handleSlideChange = (swiper)=>{
+    const currentIndex = swiper.activeIndex;
+    setProject(projects[currentIndex]); 
+  }
+
+  return <motion.section 
+  initial={{opacity:0}} 
+  animate={{opacity:1, transition:{delay:2.4, duration:0.4, ease:'easeIn'}}}
+  className="min-h-[80vh] flex flex-col justify-center py-12 lg:px-0"
+  >
+    <div className="container mx-auto">
+      <div className="flex flex-col lg:flex-row lg:gap-[30px]">
+        <div className="w-full lg:w-[50%] lg:h-[460px] flex flex-col lg:justify-between order-2 lg:order-1">
+          <div className="flex flex-col gap-[30px] h-[50%]">
+            <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+              {project.num}
+            </div>
+            <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+              {project.category} project
+            </h2>
+            <p className="text-white/60">{project.description}</p>
+            <ul className="flex flex-wrap gap-4">
+              {project.stack.map((item, index)=> {
+                return <li key={index} className="text-xl text-accent">
+                  {item.name}
+                  {index !== project.stack.length - 1 && ','}
+                </li>
+              })}
+            </ul>
+            <div className="border border-white/20"></div>
+            <div className="flex items-center gap-4">
+              {project.live && <Link href={project.live} target="_blank">
+                <TooltipProvider delayDuration={100}>
+                  <Tooltip>
+                    <TooltipTrigger className='w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group hover:cursor-pointer'>
+                      <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      Live Project
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </Link>}
+              {project.github && <Link href={project.github} target="_blank">
+                <TooltipProvider delayDuration={100}>
+                  <Tooltip>
+                    <TooltipTrigger className='w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group hover:cursor-pointer'>
+                      <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      Github Repository
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </Link>}
+            </div>
+          </div>
+        </div>
+        <div className="w-full lg:w-[50%] order-1 lg:order-2">
+          <Swiper
+          spaceBetween={30}
+          slidesPerView={1}
+          onSlideChange={handleSlideChange}
+          className='lg:[520px] mb-12'
+          >
+            {projects.map((item, index)=> {
+              return <SwiperSlide key={index} className='w-full'>
+                <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                  <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                  <div className="relative w-full h-full">
+                    <Image src={item.image} fill className="object-cover" alt={item.description} />
+                  </div>
+                </div>
+              </SwiperSlide>
+            })}
+            <WorkSliderBtns containerStyles='flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] lg:bottom-0 z-20 w-full justify-between lg:w-max lg:justify-none' btnStyles='bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all cursor-pointer' />
+          </Swiper>
+        </div>
+      </div>
+    </div>
+  </motion.section>;
 }
 
-export default Works
+export default Works;
